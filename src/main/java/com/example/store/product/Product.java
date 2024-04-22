3fb52e69045465cfb16965923bc69d57c497c5d4
SO YEON LEE <lana_75@naver.comgit>
2024-04-22 16:05:17 +0900

    ìproduct 테이블 생성

---
+++ src/main/java/com/example/store/product/Product.java
@@ -0,0 +1,43 @@
+package com.example.store.product;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Data
+@Table(name = "product_tb")
+@Entity
+public class Product {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @Column(unique = true, length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer qty;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Builder
+
+    public Product(Integer id, String name, Integer price, Integer qty, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+        this.createdAt = createdAt;
+    }
+}
+
